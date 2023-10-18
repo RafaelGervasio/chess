c71ec0b32df05afa6fa19e543dd9dccc4eaf16b3
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-10-18 15:11:43 +0100

    Added moving the knights functionality

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 class Board
     attr_accessor :grid, :white_king, :white_queen, :white_rook, :white_bishop, :white_knight, :white_pawn, :black_king, :black_queen, :black_rook, :black_bishop, :black_knight, :black_pawn
 
+    POSSIBLE_KNIGHT_MOVES = [[1, 2] , [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]].freeze
+
     def initialize
         @grid = Array.new(8) {Array.new(8) {'□'}}
         @white_king = "♔"
@@ -45,29 +47,144 @@ class Board
             puts row.join(' ')
         end
     end
+    def update_board(piece, row, collumn, turn)
+        turn == 'white' ? piece = white_knight : piece = black_knight
+        current_position = find_knight(turn)
+        grid[row][collumn] = piece
+        grid[current_position[0]][current_position[1]] = '□'
+        display_board()
+    end
+
+    def legal_move?(piece, row, collumn, turn)
+        if piece == 'n'
+            unless check_knight(row, collumn, turn)
+               return false
+            end
+        elsif piece == 'p'
+        end
+        end
+
+
+        check = true
+        #check univeral rules, if they don't pass, return false and stop function
+        
+        grid[row].nil? ? check = false : ''
+        check == false ? '' : grid[row][collumn].nil? ? check = false : ''
+        
+        #- You can’t make a move that results in you being in check next turn
+            # - A pinned knight
+            # - A king in check - you can’t move another pice unless it gets you out of check
+        # You can’t go trough a piece (unless if you’re a knight)
+
+        #check piece based rules, if they don't pass, return false and stop function
+
+
+        #return true
+        check
+    end
+
+    def check_knight(row, collumn, turn)
+        knight_pos = find_knight(turn)
+        if knight_pos == false
+            return false
+        end
+
+        POSSIBLE_KNIGHT_MOVES.each do |combo|
+            if knight_pos[0] + combo[0] == row && knight_pos[1] + combo[1] == collumn
+                return true
+            end
+        end
+        false
+    end
+    def find_knight(turn)
+        turn == 'white' ? piece = white_knight : piece = black_knight
+        grid.each_with_index do |row, i|
+            if row.index(piece).nil?
+                next
+            else
+                j = row.index(piece)
+                return [i, j]
+            end
+        end
+        return false
+    end
+
+
+    def take_piece?
+        #check if a player is taking a piece
+    end
 
 end
 
 class Players
-    def initialize (piece_color, name)
+    attr_reader :piece_color, :name 
+    def initialize (piece_color)
         @piece_color = piece_color
-        @name = name
     end
 end
 
 class Game
+    attr_reader :p1, :p2, :board
+    attr_accessor :turn
+
     def initialize
         @board = Board.new
-        @p1 = Players.new('white', set_player_name(1))
-        @p2 = Players.new('black', set_player_name(2))
+        @p1 = Players.new('white')
+        @p2 = Players.new('black')
+        @turn = p1.piece_color
     end
 
-    def set_player_name(number)
-        puts "Player #{number}, what's your name?"
-        gets.chomp
+    def switch
+        turn == p1.piece_color ? turn = p2.piece_color : turn = p1.piece_color
+    end
+
+    def play_game
+        setup()
+        #until game_over?
+            play_round()
+    end
+
+    def setup
+        #intro message
+        #create create players
+    end
+
+    def play_round
+        board.display_board
+        puts "-------"
+        puts "-------"
+        puts "-------"
+
+        puts "It's #{turn}'s turn. Make a move"
+        puts "-------"
+        puts "-------"
+        
+        puts "Choose piece (k, q, r, b, n, p): "
+        piece = gets.chomp
+        puts "Choose row: "
+        row = 7 - gets.chomp.to_i
+        puts "Choose collumn: "
+        collumn = gets.chomp.to_i
+        
+        puts "-------"
+        puts "-------"
+
+        if board.legal_move?(piece, row, collumn, turn)
+            board.update_board(piece, row, collumn, turn)
+            turn.switch
+        end
+
+
+        #If board.legal_move?(piece, row, collumn, turn)
+            #Make the move, aka, update the borard
+            #If take piece, update the board too.
+        #If not, display move not allowed error, and recall playround
+        #Change the turn
     end
 end
 
 
-b = Board.new
-b.display_board
+g = Game.new
+g.play_round
+g.play_round
+# g.board.display_board
